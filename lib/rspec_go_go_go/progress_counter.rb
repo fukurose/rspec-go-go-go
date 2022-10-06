5b72458d3e241e4fd63753af4cb32951b0a4eb99
fukurose <t.fukurose@ehealthcare.co.jp>
2022-10-06 15:51:18 +0900

    add progress counter

---
+++ lib/rspec_go_go_go/progress_counter.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module RSpecGoGoGo
+  class ProgressCounter
+    def initialize(total)
+      @total = total
+      @passes = 0
+      @failures = 0
+    end
+    attr_reader :total, :passes, :failures
+
+    def passed
+      @passes += 1
+    end
+
+    def failed
+      @failures += 1
+    end
+
+    def checks
+      @passes + @failures
+    end
+
+    def rate
+      (checks.to_f / total).round(2)
+    end
+  end
+end
