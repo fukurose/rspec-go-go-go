90a36b626466e6097c4f3ed131112f7739ad9ad1
fukurose <t.fukurose@ehealthcare.co.jp>
2022-10-06 18:12:37 +0900

    add formater

--- lib/rspec_go_go_go.rb
+++ lib/rspec_go_go_go.rb
@@ -1,70 +1,28 @@
 # frozen_string_literal: true
 
 require_relative "rspec_go_go_go/version"
+require_relative "rspec_go_go_go/progress_counter"
+require_relative "rspec_go_go_go/progress_framer"
 
-require 'rspec/core/formatters/base_text_formatter'
+require "rspec/core/formatters/base_text_formatter"
 
-module RspecGoGoGo
+module RSpecGoGoGo
+  class Formater < RSpec::Core::Formatters::BaseTextFormatter
+    RSpec::Core::Formatters.register self, :start, :example_passed, :example_failed
 
-  class Error < StandardError; end
-  # Your code goes here...
-
-  class Format < RSpec::Core::Formatters::BaseTextFormatter
-    class Progress
-      BAR_WIDTH = 80
-  
-  	  def initialize(total)
-        @total = total
-  	    @pass = 0
-  	    @failure = 0
-      end
-  
-      def passed
-        @pass += 1
-        bar
-      end
-  
-      def failed
-        @failure += 1
-        bar
-      end
-  
-      private
-  
-      def count
-        @pass + @failure
-      end
-  
-      def curent
-        (@pass + @failure).to_f / @total
-      end
-  
-      def bar
-        percent = curent * 100.0
-        bar_len = curent * BAR_WIDTH.to_f
-        bar_str = "|" + ('=' * bar_len).ljust(BAR_WIDTH) + "|"
-        progress_num = '%3.1f' % percent
-        "\r #{(@pass + @failure)} examples, #{@failure} failure #{bar_str} #{'%5s' % progress_num}%"
-      end
-    end
-  
-  	RSpec::Core::Formatters.register self, :start, :example_passed, :example_failed
-        
-  	def initialize(output)
-      super
-  	end
-  
     def start(notification)
       super
-      @progress = Progress.new(notification.count)
+      @counter = ProgressCounter.new(notification.count)
     end
-  
-  	def example_passed(notification)
-      @output << @progress.passed
-  	end
-        
-  	def example_failed(notification)
-      @output << @progress.failed
-  	end
+
+    def example_passed(_notification)
+      @counter.passed
+      @output << ProgressFramer.display(@counter)
+    end
+
+    def example_failed(_notification)
+      @counter.failed
+      @output << ProgressFramer.display(@counter)
+    end
   end
 end
