c2ef4ca2d95f3f3041418d8ae5d93f85281e61ac
fukurose <t.fukurose@ehealthcare.co.jp>
2022-12-28 21:14:53 +0900

    display the example

--- lib/rspec/go_go_go/progress_counter.rb
+++ lib/rspec/go_go_go/progress_counter.rb
@@ -3,12 +3,14 @@
 module RSpec
   module GoGoGo
     class ProgressCounter
-      def initialize(total)
+      def initialize(total, display_line_for_description: 10)
         @total = total
         @passes = 0
         @failures = 0
+        @display_line_for_description = display_line_for_description
+        @recently_descriptions = Array.new(display_line_for_description, ["", :success])
       end
-      attr_reader :total, :passes, :failures
+      attr_reader :total, :passes, :failures, :recently_descriptions, :display_line_for_description
 
       def passed
         @passes += 1
@@ -24,6 +26,11 @@ module RSpec
 
       def rate
         (checks.to_f / total).round(2)
+      end
+
+      def update_recently_descriptions(description, color)
+        @recently_descriptions.shift
+        @recently_descriptions.push([description, color])
       end
     end
   end
